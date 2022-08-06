fb30af135e88530b44fd8c497e59c3972dcb56f3
tom106gunma <tom106.net@gmail.com>
2022-08-07 07:34:13 +0900

    line_bot_controller を修正

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,7 +1,38 @@
 class LineBotController < ApplicationController
-    protect_from_forgery with: :null_session
+  require "line/bot"
 
-    def callback
-        binding.pry
+  protect_from_forgery with: :null_session
+
+  def callback
+    # LINEで送られてきたメッセージのデータを取得
+    body = request.body.read
+
+    # LINE以外からリクエストが来た場合 Error を返す
+    signature = request.env["HTTP_X_LINE_SIGNATURE"]
+    unless client.validate_signature(body, signature)
+      head :bad_request and return
+    end
+
+    # LINEで送られてきたメッセージを適切な形式に変形
+    events = client.parse_events_from(body)
+
+    events.each do |event|
+      # LINE からテキストが送信された場合
+      if (event.type === Line::Bot::Event::MessageType::Text)
+        # LINE からテキストが送信されたときの処理を記述する
+      end
+    end
+
+    # LINE の webhook API との連携をするために status code 200 を返す
+    render json: { status: :ok }
+  end
+
+  private
+
+    def client
+      @client ||= Line::Bot::Client.new do |config|
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      end
     end
 end
